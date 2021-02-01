c75b221be8ba4a3a48aa36db239f497c85e50f12
ERIKA LEITE PAIVA <you@example.com>
2021-02-01 20:04:26 -0300

    Interface de Publicacao - criada

--- Interfaces/IPublicacao.cs
+++ Interfaces/IPublicacao.cs
@@ -2,6 +2,9 @@ namespace Projeto_InstaDev.Interfaces
 {
     public interface IPublicacao
     {
-         
+        void Create (Publicacao p);
+        List<Publicacao> ReadAll();
+        void Update (Publicacao p);
+        void Delete(int id);  
     }
 }
