84f624449ebe6788b67b63b7d87a0b4705c8b3c3
unknown <dannilorch@gmail.com>
2021-02-03 09:57:11 -0300

    Model e Controller feito

--- Interfaces/IEditarPerfil.cs
+++ Interfaces/IEditarPerfil.cs
@@ -6,11 +6,8 @@ namespace Projeto_InstaDev.Interfaces
     public interface IEditarPerfil
     {
         void Create(EditarPerfil u);
-
         List<EditarPerfil> ReadAll();
-
         void Update(EditarPerfil u);
-
         void Delete(EditarPerfil u);
     }
 }
