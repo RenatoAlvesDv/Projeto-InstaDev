11e5f7492e85671e044e1911b6d947dd7368e2f8
Your Name <renato_inovacao@hotmail.com>
2021-02-05 12:34:01 -0300

    perfil Finalizado

--- Controllers/PerfilController.cs
+++ Controllers/PerfilController.cs
@@ -7,37 +7,14 @@ namespace Projeto_InstaDev.Controllers
 {
     [Route("Perfil")]
     public class PerfilController : Controller
-    {
+    { 
+            Usuario usuarioModel = new Usuario();
 
-        public IActionResult Index([FromQuery] int idUsuario)
-        {
-            Usuario usuario = new Usuario();
-
-            var userId = -1;
-
-            if(idUsuario == 0)
-                userId = int.Parse(HttpContext.Session.GetString("_IdLogado"));
-            else
-                userId = idUsuario;
-
-            ViewBag.perfil = usuario.BuscarUsuarioPorId(userId); 
-
-            return View();
-        }
-
-        [Route("Listar")]
-
-        public IActionResult Listar()
-        {
-            
-            return View();
-        }
-
-        [Route("Sair")]
-        public IActionResult Sair()
-        {
-            HttpContext.Session.Remove("_IdLogado");
-            return LocalRedirect("/Login");
-        }
+            public IActionResult Index()    
+            {
+                ViewBag.Usuario = usuarioModel.ReadAll();
+                return View();
+            }
+   
     }
 }
